858f89cb3eae6a9d33639e1d889762dc077115ea
Asenf Talca <asenf.talca@gmail.com>
2026-02-05 17:11:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "centras-de-socios-398495-f9325",
+  "appId": "1:260549603649:web:31eee653769b14b379b1c0",
+  "apiKey": "AIzaSyDT_9_58DPNO6nQYpsC7-A4tEBbkf3BiCQ",
+  "authDomain": "centras-de-socios-398495-f9325.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "260549603649"
+};
