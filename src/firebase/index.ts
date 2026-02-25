de6aa100f52b27d573d3f70c01529368051dbc28
Asenf Talca <asenf.talca@gmail.com>
2026-02-25 19:01:38 +0000

    Estoy teniendo un error de Content Security Policy (worker-src 'self') a

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore, getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -33,10 +33,21 @@ export function initializeFirebase() {
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
+  let firestore;
+  try {
+    // Inicializar Firestore forzando Long Polling para evitar errores de CSP (worker-src 'self')
+    firestore = initializeFirestore(firebaseApp, {
+      experimentalForceLongPolling: true,
+    });
+  } catch (e) {
+    // Si ya ha sido inicializado por otro componente, obtener la instancia existente
+    firestore = getFirestore(firebaseApp);
+  }
+
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: firestore
   };
 }
 
