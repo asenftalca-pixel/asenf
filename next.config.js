d589abea39958198f0dbdec8b08e45467efbc929
asenftalca-pixel <asenf.talca@gmail.com>
2026-03-02 16:42:05 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  output: 'standalone', // <--- ESTO ES LO QUE GOOGLE PIDE
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
+
 module.exports = nextConfig
