85db61625331765f31d7aa94de1d47e4a0c9c38e
asenftalca-pixel <asenf.talca@gmail.com>
2026-03-02 16:10:51 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@
 const nextConfig = {
   output: 'standalone',
   typescript: {
+    // Esto evita que el error 51 detenga la App por temas de TypeScript
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Esto evita que se detenga por reglas de formato
     ignoreDuringBuilds: true,
-  }
+  },
 }
 
 module.exports = nextConfig
