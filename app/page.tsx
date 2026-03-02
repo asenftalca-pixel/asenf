05ce58b2d35bb4ccc3283ca441a637a01bc827ad
asenftalca-pixel <asenf.talca@gmail.com>
2026-03-02 16:06:38 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>ASENF Talca - Sistema en Línea</h1>
+      <p>La plataforma se está configurando correctamente.</p>
+    </div>
+  )
+}
